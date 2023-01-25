171c737f54f5a93c116833ad26b8e9acba9c2684
atskayasatana <88509622+atskayasatana@users.noreply.github.com>
2023-01-25 23:44:44 +0300

    added check for redirect

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ import requests
 from pathlib import Path
 
 
+def check_for_redirect(response):
+    if response.history:
+        raise requests.exceptions.HTTPError
+
+
 if __name__ == '__main__':
 
     BASE_DIR = os.path.dirname(os.path.realpath(__file__))
@@ -11,12 +16,20 @@ if __name__ == '__main__':
     Path(os.path.join(BASE_DIR,'books')).mkdir(parents=True, exist_ok=True)
 
     for i in range(11):
-        url = f'https://tululu.org/txt.php?id={i}'
-        response = requests.get(url)
-        response.raise_for_status()
-        filename = f'books/{i}.txt'
-        with open(filename, 'wb') as file:
-            file.write(response.content)
+        try:
+            url = f'https://tululu.org/txt.php?id={i}'
+            response = requests.get(url)
+            check_for_redirect(response)
+            response.raise_for_status()
+            filename = f'books/{i}.txt'
+            with open(filename, 'wb') as file:
+                file.write(response.content)
+        except requests.exceptions.HTTPError:
+            print('Ошибка скачивания')
+
+
+
+
 
 
 
